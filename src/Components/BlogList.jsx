f99464ceb7a5aaf2e4a630fe60c61e09fd4e91ea
coder <khyathi2509@gmail.com>
2025-07-05 13:19:49 +0530

    first test

--- src/Components/BlogList.jsx
+++ src/Components/BlogList.jsx
@@ -10,7 +10,7 @@ const BlogList = () => {
   return (
     <div>
         <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>
-            {blogCategories.map((item)=> (
+            {blogCategories.map((item)=> ( 
                 <div key={item} className='relative'>
                     <button onClick={()=>setMenu(item)}
                     className={`cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}>
