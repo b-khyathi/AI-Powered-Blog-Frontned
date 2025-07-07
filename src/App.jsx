91094b014aa2e1ebeba17f7c0a2a792223d4a3dc
coder <khyathi2509@gmail.com>
2025-07-07 18:03:40 +0530

    imported { useAppContext } from context/AppContext

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import Layout from './Pages/admin/Layout'
 import Login from './Components/admin/Login'
 import 'quill/dist/quill.snow.css'
 import {Toaster} from 'react-hot-toast'
+import { useAppContext } from './context/AppContext'
+
+const {token} = useAppContext()
 
 const App = () => {
   return (
@@ -18,7 +21,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/blog/:id' element={<Blog/>}/>
-        <Route path='/admin' element={true ? <Layout/> : <Login/>}>
+        <Route path='/admin' element={token ? <Layout/> : <Login/>}>
             <Route index element={<Dashboard/>}/>
             <Route path='addBlog' element={<AddBlog/>}/>
             <Route path='listBlog' element={<ListBlog/>}/>
